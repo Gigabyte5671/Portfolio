0243fc47a3830a8111893282373aa49128c3859f
NotNatural21 <annettemorganscott@outlook.co.nz>
2023-02-07 14:45:59 +1300

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'App'
+
+})
+</script>
+
+<template>
+  
+</template>
+
+<style>
+
+
+</style>
